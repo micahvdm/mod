abcd082c2b69ec49faeb4e84500dff14c7851757
DivideCo <micahvdm@gmail.com>
2021-03-31 18:45:53 +0200

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update --fix-missing
+sudo apt-get install virtualenv python3-pip python3-dev git build-essential libasound2-dev libjack-jackd2-dev liblilv-dev libjpeg-dev zlib1g-dev liblilv-dev libreadline-dev
+
 git clone https://github.com/moddevices/mod-host
 git clone https://github.com/moddevices/mod-ui
 git clone https://github.com/micahvdm/mod
-#wget https://drive.google.com/file/d/1lWgu4jEL0AUsyYNAYgu54fr-o3XSiFIB/view?usp=sharing
 
 mkdir .lv2
-#unzip plugins.zip
-#cd plugins
-#sudo mv * $HOME/.lv2
-
-sudo apt-get install virtualenv python3-pip python3-dev git build-essential libasound2-dev libjack-jackd2-dev liblilv-dev libjpeg-dev zlib1g-dev liblilv-dev libreadline-moddevices
 
 cd mod-host
 make
@@ -22,4 +19,6 @@ sudo make -C utils
 cd ..
 cd mod
 sudo cp mod-ui.service mod-host.service /usr/lib/systemd/system
-
+#unzip plugins.zip
+#cd plugins
+#sudo mv * $HOME/.lv2
