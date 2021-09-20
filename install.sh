9279145471fed9d611358fcc0ca8295a77e3beff
DivideCo <micahvdm@gmail.com>
2021-09-20 19:57:34 +0200

    Raspberry Pi upstream update fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-sudo apt-get update --fix-missing
+sudo apt-get update --allow-releaseinfo-change --fix-missing
 
 #Install Dependancies
 sudo apt-get -y install virtualenv python3-pip python3-dev git build-essential libasound2-dev libjack-jackd2-dev liblilv-dev libjpeg-dev zlib1g-dev cmake debhelper dh-autoreconf dh-python gperf intltool ladspa-sdk libarmadillo-dev libasound2-dev libavahi-gobject-dev libavcodec-dev libavutil-dev libbluetooth-dev libboost-dev libeigen3-dev libfftw3-dev libglib2.0-dev libglibmm-2.4-dev libgtk2.0-dev libgtkmm-2.4-dev libjack-jackd2-dev libjack-jackd2-dev liblilv-dev liblrdf0-dev libsamplerate0-dev libsigc++-2.0-dev libsndfile1-dev libsndfile1-dev libzita-convolver-dev libzita-resampler-dev lv2-dev p7zip-full python3-all python3-setuptools libreadline-dev
