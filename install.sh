4fc618b2dedfd5041d456c3e912ad35e685b391d
DivideCo <micahvdm@gmail.com>
2021-11-04 10:22:05 +0200

    updated to pi arm64 image

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ sudo pip3 install pyserial==3.0 pystache==0.5.4 aggdraw==1.3.11 scandir backport
 sudo pip3 install git+git://github.com/dlitz/pycrypto@master#egg=pycrypto
 
 #Install Mod Software
-mv /home/patch/mod /home/patch/install
-mkdir /home/patch/.lv2
-mkdir /home/patch/mod
-mkdir /home/patch/data
-mkdir /home/patch/data/pedalboards
-mkdir /home/patch/data/user-files
-cd /home/patch/data/user-files
+mv /home/pi/mod /home/pi/install
+mkdir /home/pi/.lv2
+mkdir /home/pi/mod
+mkdir /home/pi/data
+mkdir /home/pi/data/pedalboards
+mkdir /home/pi/data/user-files
+cd /home/pi/data/user-files
 mkdir "Speaker Cabinets IRs"
 mkdir "Reverb IRs"
 mkdir "Audio Loops"
@@ -30,16 +30,10 @@ mkdir "MIDI Songs"
 mkdir "Hydrogen Drumkits"
 mkdir "SF2 Instruments"
 mkdir "SFZ Instruments"
-cd /home/patch/mod
+cd /home/pi/mod
 
 #Jack2
-# git clone --branch v1.9.14 https://github.com/jackaudio/jack2.git
-# cd jack2
-# ./waf configure
-# ./waf build
-# sudo ./waf install
-# ./waf clean
-# cd ..
+sudo apt-get install -y jackd2
 
 #Browsepy
 git clone https://github.com/moddevices/browsepy.git
@@ -55,36 +49,6 @@ sudo make install
 make clean
 cd ..
 
-#Mod-cabsim
-#git clone https://github.com/moddevices/mod-cabsim-IR-loader.git
-#cd mod-cabsim-IR-loader/source
-#make
-#cp -r cabsim-IR-loader.lv2 /home/patch/.lv2/
-#cd ../..
-
-#Veja cabsim
-#git clone https://github.com/VeJa-Plugins/Cabinet-Simulator.git
-#cd Cabinet-Simulator/cabsim/source
-#make
-#cp -r cabsim.lv2/ ~patch/.lv2
-#cd ../../..
-
-#Veja bass cabsim
-#git clone https://github.com/VeJa-Plugins/Bass-Cabinets.git
-#cd Bass-Cabinets
-#make clean
-#make
-#cp -r veja-bass-cab.lv2/ ~patch/.lv2
-#cd ..
-
-#tamgamp - very good sounding amp sim
-#git clone https://github.com/sadko4u/tamgamp.lv2.git
-#cd tamgamp.lv2
-#make
-#sudo make install
-#cp -r /usr/local/lib/lv2/tamgamp.lv2 ~patch/.lv2
-#cd ..
-
 #Mod-ui
 git clone --branch hotfix-1.10-filehandling https://github.com/moddevices/mod-ui.git
 cd mod-ui
@@ -95,14 +59,14 @@ make
 cd ..
 sudo ./setup.py install
 cd ..
-cp -r /home/patch/mod/mod-ui/default.pedalboard /home/patch/data
+cp -r /home/pi/mod/mod-ui/default.pedalboard /home/pi/data
 
 #Create Services
-cd /home/patch/install
+cd /home/pi/install
 sudo cp *.service /usr/lib/systemd/system/
 sudo ln -sf /usr/lib/systemd/system/browsepy.service /etc/systemd/system/multi-user.target.wants
 sudo ln -sf /usr/lib/systemd/system/mod-host.service /etc/systemd/system/multi-user.target.wants
 sudo ln -sf /usr/lib/systemd/system/mod-ui.service /etc/systemd/system/multi-user.target.wants
 sudo ln -sf /usr/lib/systemd/system/mod-monitor.service /etc/systemd/system/multi-user.target.wants
-sudo ln -s /home/patch/data /root/data
-sudo ln -s /home/patch/data/pedalboards /root/.pedalboards
+sudo ln -s /home/pi/data /root/data
+sudo ln -s /home/pi/data/pedalboards /root/.pedalboards
