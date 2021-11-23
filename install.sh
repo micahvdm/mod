2fe2bfb99083c2cc4ea15e9330fc0ab829fcc611
DivideCo <micahvdm@gmail.com>
2021-11-23 12:41:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,22 +58,22 @@ cd ..
 sudo ./setup.py install
 
 #TouchOSC2midi
-pushd $(mktemp -d) && https://github.com/BlokasLabs/touchosc2midi.git
+pushd $(mktemp -d) && git clone https://github.com/BlokasLabs/touchosc2midi.git
 pushd touchosc2midi
 sudo pip3 install ./
 
 #Amidithru
-pushd $(mktemp -d) && https://github.com/BlokasLabs/amidithru.git
+pushd $(mktemp -d) && git clone https://github.com/BlokasLabs/amidithru.git
 pushd amidithru
 sudo make install
 
 #amidiauto
-pushd $(mktemp -d) && https://github.com/BlokasLabs/amidiauto.git
+pushd $(mktemp -d) && git clone https://github.com/BlokasLabs/amidiauto.git
 pushd amidiauto
 sudo make install
 
 #mod-midi-merger
-pushd $(mktemp -d) && https://github.com/moddevices/mod-midi-merger.git
+pushd $(mktemp -d) && git clone https://github.com/moddevices/mod-midi-merger.git
 pushd mod-midi-merger
 mkdir build && cd build
 cmake ..
